26a22898abb78b1ceac6f5edc0fd393b6b1bda38
cpeddecord <cpeddecord@gmail.com>
2016-02-18 20:52:28 -0800

    new about page

--- app/components/AboutPage.js
+++ app/components/AboutPage.js
@@ -4,8 +4,9 @@ export default function AboutPage () {
   function renderAboutContent() {
     return(
       <div>
-        <h2>About</h2>
-        <p>Wholly disinterested in artist statments, Chris prefers to create work.</p>
+        <p>Former professional dancer, comissioned choreographer, internationally published photographer, full-stack engineer with Nike. I also count spiders and artist's statments to be best friends and therefore not friends of mine.</p>
+
+        <p>I'm currently not taking assignments from new clientele but feel free to reach out to me just in case at chris[at]peddecordphoto[dot]com.</p>
       </div>
     );
   }
@@ -14,18 +15,25 @@ export default function AboutPage () {
     padding: '15px'
   };
 
+  const mediumHeadingStyle = {
+    textAlign: 'center'
+  }
+
   return (
     <div className='row'>
 
-      <div style={bodyTextStyle} className='small-12 medium-6 show-for-medium'>
+      <h2 style={mediumHeadingStyle} className='medium-12 show-for-medium'>About</h2>
+
+      <div style={bodyTextStyle} className='medium-offset-1 medium-5 show-for-medium'>
         {renderAboutContent()}
       </div>
 
-      <div className='small-12 medium-6 epic-hat'>
+      <div className='medium-5 epic-hat'>
         <img src="https://prismic-io.s3.amazonaws.com/peddecordphoto%2F6aeed9cc-f9db-4857-8ff3-567619066d96_epichat.jpg" />
       </div>
 
       <div style={bodyTextStyle} className='small-12 medium-6 show-for-small-only'>
+        <h1>About</h1>
         {renderAboutContent()}
       </div>
 
