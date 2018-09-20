27c3b9bcd7e851887da930ea16b3a53780c0f3b6
Chris Peddecord <cpeddecord@gmail.com>
2018-09-20 09:55:51 -0400

    echoing to http vs stdout

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,7 @@ import * as Koa from 'koa';
 const app = new Koa();
 
 app.use(async (ctx) => {
-  console.log(ctx.request.href);
-  ctx.body = 'hello world';
+  ctx.body = `hello world: ${ctx.request.href}`;
 });
 
 console.log('Koa at 3000');
