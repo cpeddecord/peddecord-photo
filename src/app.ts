ca63951aefb186d1eb9cb1d150d991316790bc38
Chris Peddecord <cpeddecord@gmail.com>
2018-09-19 22:35:01 -0400

    woop

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import * as Koa from 'koa';
 const app = new Koa();
 
 app.use(async (ctx) => {
+  console.log(ctx.request.href);
   ctx.body = 'hello world';
 });
 
+console.log('Koa at 3000');
 app.listen(3000);
