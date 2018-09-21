c6f22151043bc5009f32ebe21503c44c26065fea
Chris Peddecord <cpeddecord@gmail.com>
2018-09-21 18:52:45 -0400

    adding some bread to that healthz route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  console.log(ctx.request.headers);
-  ctx.body = `<!DOCTYPE html><html><head><meta name="google-site-verification" content="LIA0G4bJQIQGiM8VX3vHKIJf_yRMiG8TQqQKRPkkJ2g" /></head><body>hello world: ${
-    ctx.request.href
-  }</body></html>`;
-});
+const temporaryTemplate = (s: string): string => {
+  return `<!DOCTYPE html><html><head><meta name="google-site-verification" content="LIA0G4bJQIQGiM8VX3vHKIJf_yRMiG8TQqQKRPkkJ2g" /></head><body>hello world: ${s}</body></html>`;
+};
+
+router.get(
+  '/healthz',
+  async (ctx): Promise<any> => {
+    ctx.body = '🍞';
+  }
+);
+
+router.get(
+  '/*',
+  async (ctx): Promise<any> => {
+    console.log(ctx.request.headers);
+    ctx.body = temporaryTemplate(ctx.request.href);
+  }
+);
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000);
 
 console.log('Koa at 3000');
-app.listen(3000);
