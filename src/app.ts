18066b15b2557637b2a64b931af1861ed22990d6
Chris Peddecord <cpeddecord@gmail.com>
2018-09-21 18:24:05 -0400

    uhh

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import * as Koa from 'koa';
 const app = new Koa();
 
 app.use(async (ctx) => {
+  console.log(ctx.request.headers);
   ctx.body = `<!DOCTYPE html><html><head><meta name="google-site-verification" content="LIA0G4bJQIQGiM8VX3vHKIJf_yRMiG8TQqQKRPkkJ2g" /></head><body>hello world: ${
     ctx.request.href
   }</body></html>`;
