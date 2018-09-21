50378b800c621cda8655ecbdf9f03250dc38d054
Chris Peddecord <cpeddecord@gmail.com>
2018-09-21 19:27:27 -0400

    removing healthcheck for now in spite of bread, adding http redirect layer

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 
+const DO_REDIRECTS = process.env.DO_REDIRECTS || false;
+
+const HOST_HEADER = 'host';
+const PROTOCOL_HEADER = 'x-forwarded-proto';
+
 const app = new Koa();
 const router = new Router();
 
@@ -18,12 +23,29 @@ router.get(
 router.get(
   '/*',
   async (ctx): Promise<any> => {
-    console.log(ctx.request.headers);
-    ctx.body = temporaryTemplate(ctx.request.href);
+    ctx.body = temporaryTemplate(ctx.request.path);
   }
 );
 
+async function redirect(ctx, next) {
+  if (!DO_REDIRECTS) {
+    return next();
+  }
+
+  const host = ctx.headers[HOST_HEADER];
+  const protocol = ctx.headers[PROTOCOL_HEADER];
+  const path = ctx.path;
+
+  if (!host.includes('www.') || protocol !== 'https') {
+    ctx.status = 301;
+    ctx.redirect(`https://www.peddecord.photo${path}`);
+  } else {
+    return next();
+  }
+}
+
 app
+  .use(redirect)
   .use(router.routes())
   .use(router.allowedMethods())
   .listen(3000);
